a50292b5c1b45c087f2e8bc9329fe8fddf5ad7e8
Yevgen <osadchyi15@gmail.com>
2024-08-22 00:07:36 +0200

    movies

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -3,7 +3,12 @@ import css from "./LoadMoreBtn.module.css";
 const LoadMoreBtn = ({ onClickMoreBtn, isLoading }) => {
   return (
     <div>
-      <button type="button" onClick={onClickMoreBtn} disabled={isLoading}>
+      <button
+        className={css.loadMoreButton}
+        type="button"
+        onClick={onClickMoreBtn}
+        disabled={isLoading}
+      >
         {isLoading ? "Wait for it. Loading!" : "Load more"}
       </button>
     </div>
